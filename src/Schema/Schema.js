c0cfc3885272cb90079bd3ce3d00d1c3d66b7c42
Vishrut Reddi <vishrutreddi@gmail.com>
2017-12-22 19:38:04 -0800

    Add state to identify if a user is logged in or not

--- src/Schema/Schema.js
+++ src/Schema/Schema.js
@@ -2,7 +2,7 @@ const graphql = require('graphql');
 const { GraphQLSchema } = graphql;
 const mutation = require('./Mutations/Auth');
 
-const RootQueryType = require('./types/RootQueryType');
+const RootQueryType = require('./Types/RootQueryType');
 
 module.exports = new GraphQLSchema({
   query: RootQueryType,
